cf398298b6cce235a2a28965bc7d12be89675fa0
Azharafridi <99399706+Azharafridi@users.noreply.github.com>
2025-04-09 12:23:33 +0500

    done initial library importing, generating function

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+import os 
+from dotenv import load_dotenv
+load_dotenv()
